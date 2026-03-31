397fa43bbe6d283e07ab97576f5a3b9bed729755
YJ Shin <ksyj115@naver.com>
2026-03-31 21:48:13 +0900

    add comment

--- src/io/member/impl/FileMemberRepository.java
+++ src/io/member/impl/FileMemberRepository.java
@@ -16,6 +16,12 @@ public class FileMemberRepository implements MemberRepository {
 
     @Override
     public void add(Member member) {
+
+        // java 7 부터 도입된 try-with-resources 문법으로,
+        // try 뒤의 괄호 (...) 안에서 선언된 객체는 AutoCloseable 인터페이스를 구현하고 있기 때문에,
+        // 블록이 정상적으로 끝나거나 중간에 예외가 발생하더라도 무조건 close() 를 호출하도록 설계되어 있다.
+
+                                                    // UTF_8 (인코딩 캐릭터셋) 생략시 시스템 기본 인코딩 캐릭터셋 사용, true 이면 해당 파일의 마지막에 추가로 저장.
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH, UTF_8, true))) {
             bw.write(member.getId() + DELIMITER + member.getName() + DELIMITER + member.getAge());
             bw.newLine();
